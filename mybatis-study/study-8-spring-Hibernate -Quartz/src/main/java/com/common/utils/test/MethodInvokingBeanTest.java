e1e529192dbd31e4e8dec27e59b431b54465da0d
汪吉 <983433479@qq.com>
2017-10-12 19:08:04 +0800

    [add]spring MethodInvokingFactoryBean的简单使用

---
+++ mybatis-study/study-8-spring-Hibernate -Quartz/src/main/java/com/common/utils/test/MethodInvokingBeanTest.java
@@ -0,0 +1,17 @@
+package com.common.utils.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * descrption:
+ * authohr: wangji
+ * date: 2017-08-24 14:20
+ */
+@Slf4j
+public class MethodInvokingBeanTest {
+
+
+    public static void test(){
+        log.info("调用了这个方法");
+    }
+}
